8dc1852bd510477dd5fd27479b16b6c31933855f
thomaslian <13934813+thomaslian@users.noreply.github.com>
2022-03-04 16:54:29 +0100

    generateKeys function returns public key and wif instead of hex

--- functions/bitcoin.js
+++ functions/bitcoin.js
@@ -5,16 +5,19 @@ const admin = require('firebase-admin');
 // Import ecpair to generate Bitcoin keys.
 const { ECPairFactory } = require('ecpair');
 const ecc = require('tiny-secp256k1');
+const bitcoin = require('bitcoinjs-lib');
 
 /**
  * Generate a new Bitcoin public key and private key
  */
- exports.generateKeys = functions.region('europe-west3').https.onCall(async (data, context) => {
-    const ECPair = ECPairFactory(ecc)
+
+exports.generateKeys = functions.region('europe-west3').https.onCall((data, context) => {
+
+    const ECPair = ECPairFactory(ecc);
     const keyPair = ECPair.makeRandom();
 
-    const pubKey = keyPair.getPublicKeyBuffer().toString('hex');
-    const privKey = keyPair.getPrivateKeyBuffer().toString('hex');
+    const pubKey = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey }).address;
+    const privKey = keyPair.toWIF();
 
     return { pubKey, privKey };
 });
--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,8 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 const bitcoin = require('./bitcoin');
+exports.generateBitcoinKeys = bitcoin.generateKeys;
