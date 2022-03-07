c6e2917bd754992c437f241a53c4d313218ac569
thomaslian <13934813+thomaslian@users.noreply.github.com>
2022-03-07 20:33:26 +0100

    private keys gets encrypted and stored in firestore. Created functions for encrypting and decrypting text

--- functions/bitcoin.js
+++ functions/bitcoin.js
@@ -6,17 +6,31 @@ const admin = require('firebase-admin');
 const { ECPairFactory } = require('ecpair');
 const ecc = require('tiny-secp256k1');
 const bitcoin = require('bitcoinjs-lib');
+const { encrypt, decrypt } = require('./crypto');
 
 /**
  * Generate a new Bitcoin public key and private key
  */
 exports.generateKeys = functions.region('europe-west3').https.onCall((data, context) => {
+    const uid = context.auth.uid;
+    const password = data.userPassword;
 
     const ECPair = ECPairFactory(ecc);
     const keyPair = ECPair.makeRandom();
 
     const publicKey = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey }).address;
-    const privateKey = keyPair.toWIF();
+    const unencryptedPrivateKey = keyPair.toWIF();
+    
+    const privateKey = encrypt(unencryptedPrivateKey, password);
+
+    admin.firestore().collection('users').doc(uid).set({
+        uid,
+        publicKey,
+        privateKey
+    }).catch(error => {
+        console.log(error);
+        return error;
+    });
 
     return { publicKey, privateKey };
 });
---
+++ functions/crypto.js
@@ -0,0 +1,25 @@
+var crypto = require("crypto-js");
+
+function encrypt(text, password) {
+    const salt = crypto.lib.WordArray.random(128 / 8);
+    const key = crypto.PBKDF2(password, salt);
+    const iv = crypto.lib.WordArray.random(128 / 8);
+    const encrypted = crypto.AES.encrypt(text, key, { iv: iv });
+
+    return salt.toString() + iv.toString() + encrypted.toString();
+}
+
+function decrypt(text, password) {
+    const salt = crypto.enc.Hex.parse(text.substring(0, 32));
+    const iv = crypto.enc.Hex.parse(text.substring(32, 64));
+    const encrypted = text.substring(64);
+    const key = crypto.PBKDF2(password, salt);
+    const decrypted = crypto.AES.decrypt(encrypted, key, { iv: iv });
+
+    return decrypted.toString(crypto.enc.Utf8);
+}
+
+module.exports = {
+    encrypt, 
+    decrypt
+}
